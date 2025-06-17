212820ae32a2b58f595598bcb49cda168c4d8354
FastLeopard8414 <alejandro.ranero@gmail.com>
2025-06-17 19:11:25 +0200

    Add files via upload

---
+++ player ui.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class playerui : MonoBehaviour    
+{
+    public PlayerPrefs player;
+    public TextMeshProUGUI coinsCounterText;
+    public Slider healthSlider;
+}           
